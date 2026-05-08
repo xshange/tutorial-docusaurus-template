356ceb46a48800d81f5e239882edabb7143d5837
Manuel Kaufmann <humitos@gmail.com>
2026-05-08 13:12:46 +0200

    Initial Docusaurus project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Template for the Read the Docs Docusaurus tutorial
+
+This GitHub template includes a fictional Python library
+with some basic Docusaurus docs.
+
+Read the tutorial here:
+
+https://docs.readthedocs.io/en/stable/tutorial/docusaurus.html
---
+++ docusaurus.config.js
@@ -0,0 +1,78 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Lumache',
+  tagline: 'A Python library for cooks and food lovers',
+  favicon: 'img/favicon.ico',
+
+  future: {
+    v4: true,
+  },
+
+  url: 'https://your-docusaurus-site.example.com',
+  baseUrl: '/',
+
+  // Required for compatibility with Read the Docs.
+  // See https://docs.readthedocs.io/en/stable/intro/docusaurus.html
+  trailingSlash: true,
+
+  onBrokenLinks: 'throw',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          // Serve the docs at the site's root.
+          routeBasePath: '/',
+          sidebarPath: './sidebars.js',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        respectPrefersColorScheme: true,
+      },
+      navbar: {
+        title: 'Lumache',
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Docs',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: 'Built with Docusaurus and hosted on Read the Docs.',
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
